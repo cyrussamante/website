21784c32bc432f75ad3098f191d771a95c370665
cyrussamante <cyruss_amante@yahoo.ca>
2025-06-11 10:58:42 -0400

    refactor: remove mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,10 @@
-import { AppShell, Burger, NavLink} from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-
-
 import './App.css'
 
 function App() {
-    const [opened, { toggle }] = useDisclosure();
-
 
   return (
     <>
-      <AppShell
-        header={{ height: 60 }}
-        navbar={{
-          width: 300,
-          breakpoint: 'sm',
-          collapsed: { mobile: !opened },
-        }}
-        padding="md"
-      > 
-
-      <AppShell.Header>
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          hiddenFrom="sm"
-          size="sm"
-        />
-        <div>cyruss amante</div>
-      </AppShell.Header>
-
-      <AppShell.Navbar p="md">
-        <AppShell.Section>Cyruss Amante</AppShell.Section>
-        <AppShell.Section>
-          <NavLink
-            href="#required-for-focus"
-            label="about"
-            variant="subtle"
-          />
-          <NavLink
-            href="#required-for-focus"
-            label="projects"
-            variant="subtle"
-          />
-          <NavLink
-            href="#required-for-focus"
-            label="experience"
-            variant="subtle"
-          />
-          <NavLink
-            href="#required-for-focus"
-            label="contact"
-            variant="subtle"
-          />
-        </AppShell.Section>
-      </AppShell.Navbar>
-
-      <AppShell.Main>
-          <h1>my content</h1>
-          <p>content over here</p>
-      </AppShell.Main>
-
-      </AppShell>
-  
+     <h1>test</h1>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ColorSchemeScript, MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ColorSchemeScript defaultColorScheme="dark" />
-    <MantineProvider defaultColorScheme="dark">
-      <App />
-    </MantineProvider>
+    <App />
   </StrictMode>,
 )
