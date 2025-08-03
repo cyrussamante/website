8f77076295735c9d81641fe0a51ece8f94a39f96
cyrussamante <cyruss_amante@yahoo.ca>
2025-08-03 13:05:40 -0400

    wip: add navlinks to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,25 @@ import "./Navbar.css";
 
 function Navbar() {
   return (
-    <>
-      <h2>Cyruss.</h2>
-    </>
+    <nav>
+      <a href="/" id="logo">
+        Cyruss<span id="dot">.</span>
+      </a>
+      <ul className="nav-list">
+        <li className="nav-item">
+          <a href="">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="">About</a>
+        </li>
+        <li className="nav-item">
+          <a href="">Experience</a>
+        </li>
+        <li className="nav-item">
+          <a href="">Projects</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
